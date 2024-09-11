b44a27e56ba09484b915945bfdcfc4b22777db78
Miguel Estrada <miguelp.estradam@gmail.com>
2024-09-11 21:04:40 +0100

    FEAT (Hello Cargo) 1.1: First project created with cargo

---
+++ hello_cargo/target/debug/deps/hello_world-df853cdafd9ed411.d
@@ -0,0 +1,5 @@
+/home/migestrada/miguel/git/rusk-book/hello_world/target/debug/deps/hello_world-df853cdafd9ed411: src/main.rs
+
+/home/migestrada/miguel/git/rusk-book/hello_world/target/debug/deps/hello_world-df853cdafd9ed411.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/migestrada/miguel/git/rusk-book/hello_world/target/debug/hello_world: /home/migestrada/miguel/git/rusk-book/hello_world/src/main.rs
